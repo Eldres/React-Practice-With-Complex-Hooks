63e69d1b6783a66c775941e29d3654d0e82a1757
Josh Nagel <nageljp27@gmail.com>
2022-07-08 09:16:42 -0600

    implemented useEffect cleanup func

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,18 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      console.log("check form validity timeout");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    // clean-up function for useEffect()
+    return () => {
+      console.log("cleanup from useEffect()");
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
